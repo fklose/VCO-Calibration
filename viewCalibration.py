8c7fa13211741490bcc44d8ed7b8f144bb9b1a8d
Felix <felix.f.klose@gmail.com>
2022-12-29 18:58:46 -0500

    Made some changes

--- viewCalibration.py
+++ viewCalibration.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 from numpy import loadtxt
 
-V_out, V_meas, V_meas_std, F = loadtxt("calibration_upsweep.csv", unpack=True, delimiter=",")
-# V_out, V_meas, V_meas_std, F = loadtxt("calibration_downsweep.csv", unpack=True, delimiter=",")
+V_out, V_meas, V_meas_std, F = loadtxt("./Raw Calibration - 30 June 2022/calibration_upsweep.csv", unpack=True, delimiter=",")
+# V_out, V_meas, V_meas_std, F = loadtxt("./Raw Calibration - 30 June 2022/calibration_downsweep.csv", unpack=True, delimiter=",")
 
 plt.plot(V_meas, F, ls="", marker=".")
 plt.xlabel("VCO Voltage [V]")
