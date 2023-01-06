e638339756603ea435746a02436d698b23c36c7c
Felix <felix.f.klose@gmail.com>
2023-01-05 17:04:31 -0800

    Removed some unnecessary files

--- Complete Calibration - 30 June 2022/averageCalibrations.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-df, down = np.loadtxt("calibration_downsweep.csv", unpack=True)
-uf, up = np.loadtxt("calibration_upsweep.csv", unpack=True)
-
-down = np.asarray([d for _, d in sorted(zip(df, down))])
-
-np.savetxt("average.csv", list(zip(uf, (up + down)/2)))
